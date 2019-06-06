a93bde2e17b19164d51d143325b240124c452f1c
WexyWex <51077355+WexyWex@users.noreply.github.com>
2019-06-06 16:58:41 +0300

    FirstCoomit

---
+++ FreqDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ll
+{
+    class FreqDictionary
+    {
+        public static Dictionary<string, int> GetFrequenceDictionary(string text)
+        {
+            //text.Split(new char[] { '.', ',', '\'', '?', ' ', '\t', '\n', ':', ';', '!', '-' })
+            List<string> words = Regex.Split(text.ToLower(), @"(\a\r)?\s+|\W+|\d+").ToList();
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i] == "")
+                {
+                    words.RemoveAt(i);
+                }
+            }
+            return words.GroupBy(g => g).Select(s => new Tuple<string, int>(s.Key, s.Count())).ToDictionary(x => x.Item1, x => x.Item2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary;
+            Console.Write("Введите путь к файлу !  ");
+
+            string text = File.ReadAllText(Console.ReadLine());
+
+            dictionary = FreqDictionary.GetFrequenceDictionary(text);
+
+            List<string> keys = new List<string>(dictionary.Keys);
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                Console.WriteLine(keys[i] + "\t" + dictionary[keys[i]]);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
